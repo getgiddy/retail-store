78b99f887c9b029a1b8dec205828984603390c56
Gideon Anyalewechi <ganyalewechi1997@gmail.com>
2020-11-20 11:19:00 +0100

    Created models - Product, Cart, CartItem, Order, OrderItem Configured settings and urls to work with media files in dev. Added pillow to project

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core import models
 
-# Register your models here.
+admin.site.register(models.Product)
+admin.site.register(models.Cart)
+admin.site.register(models.CartItem)
+admin.site.register(models.Order)
+admin.site.register(models.OrderItem)
--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    image = models.ImageField(default="product_images/default.png",
+                              upload_to="product_images")
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}'s cart"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(to=Cart, on_delete=models.CASCADE)
+    product = models.OneToOneField(to=Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"Cart-Item - {self.quantity} x {self.product} in {self.cart}"
+
+
+ORDER_STATUSES = (
+    ('nw', 'New'),
+    ('ot', 'On Transit'),
+    ('de', 'Delivered'),
+    ('cc', 'Cancelled'),
+)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=2, choices=ORDER_STATUSES, default='nw')
+    order_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username}'s order"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE)
+    product = models.OneToOneField(to=Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"Order-Item - {self.quantity} x {self.product} in {self.order}"
